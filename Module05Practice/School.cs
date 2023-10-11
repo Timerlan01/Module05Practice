50144db5e01db4fdcd8cc96dc51de698a17e17c0
Timerlan01 <alimbekovtimerlan@gmail.com>
2023-10-11 12:19:09 +0600

    Добавьте файлы проекта.

---
+++ Module05Practice/School.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module05Practice
+{
+    public class School
+    {
+        public void CallStudentMethod()
+        {
+            Student student = new Student();
+            try
+            {
+                student.DoSomething();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Возникло исключение: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Module05Practice/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module05Practice
+{
+    public class Student
+    {
+        public void DoSomething()
+        {
+            // Генерируем исключение
+            throw new Exception("Произошла ошибка в методе DoSomething класса Student.");
+        }
+    }
+}
